c01d8bb7600d68ee2ddd9885baea588ad9edde3e
twinmagix <Sazindwandwe1997@gmail.com>
2022-03-15 11:43:06 +0200

    first commit

---
+++ 0x00-math_complex/7-division.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+void division(complex c1, complex c2, complex *c3)
+{
+    c3->re = (c1.re * c2.re + c1.im * c2.im) / (c2.re * c2.re + c2.im * c2.im);
+    c3->im = (-c1.re * c2.im + c1.im * c2.re) / (c2.re * c2.re + c2.im * c2.im);
+}
+
---
+++ 0x00-math_complex/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void display_complex_number(complex c);
+complex conjugate(complex c);
+double modulus(complex c);
+double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg (double m, double arg, complex *c3);
+
+#endif
