5e131ff567b89c12738923575361957bf55639d4
twinmagix <Sazindwandwe1997@gmail.com>
2022-03-29 10:56:36 +0200

    first commit

---
+++ 0x01-math_sequence/0-heron.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * heron - funtion that return the Heron sequence until having convergence
+ * with an error less or equal to 10^(-7)
+ *
+ * @p: Parameter with the number to compare the root
+ * @x0: Parameter with the start point
+ *
+ * Return: The head of the created linked
+ */
+t_cell *heron(double p, double x0)
+{
+	double x1, error = 0;
+	t_cell *new, *head = NULL, *tmp = NULL;
+
+	x1 = x0;
+
+	new = malloc(sizeof(t_cell));
+	if (new == NULL)
+		return (NULL);
+	head = new;
+	new->elt = x1;
+	new->next = NULL;
+
+	error = ((x1 * x1) > p) ? (x1 * x1) - p : p - (x1 * x1);
+
+	if (error <= 0.0000001)
+	{
+		return (head);
+	}
+
+	x1 = ((p / x0) + x0) / 2;
+
+	head = heron(p, x1);
+
+	tmp = head;
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+
+	tmp->next = new;
+
+	return (head);
+}
---
+++ 0x01-math_sequence/README.md
@@ -0,0 +1,14 @@
+Welcome maths sequence!!!
+
+0-heron.c
+1-fibonacci.c
+2-mandelbrot.c 
+mandelbrot.pgm 
+complex.c
+3-julia.c
+julia1.pgm
+julia2.pgm
+julia3.pgm
+julia4.pgm
+julia5.pgm
+julia6.pgm
