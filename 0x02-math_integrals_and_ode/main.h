da57f3d1af1722471a8fe7fbdc9f91b98a20415e
twinmagix <sazindwandwe1997@gmail.com>
2022-07-07 10:50:28 +0200

    int commit

--- 0x02-math_integrals_and_ode/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-double rectangle_method(double a, double b, int steps );
-double simpson(double a, double b, int steps );
-void Euler(FILE *euler, char *name,double x0, double y0, double T, int n).
-
-#endif
